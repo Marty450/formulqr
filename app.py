4353336f10e49ee75a0e592b10d799b5fdf4654b
Marty450 <kavrakovmartin3@gmail.com>
2026-02-04 13:49:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("Тест")
 
 # Въпрос 1
 st.write("Коя е столицата на България?")
-user_answer1 = st.text_input("Вашият отговор:")
+user_answer1 = st.text_input("Вашият отговор:",key ="w")
 if st.button("Провери отговор 1"):
     if user_answer1.strip().lower() == "софия":
         st.success("Верен отговор!")
@@ -13,7 +13,7 @@ if st.button("Провери отговор 1"):
 
 # Въпрос 2
 st.write("Кога е създадена България?")
-user_answer2 = st.text_input("Вашият отговор:")
+user_answer2 = st.text_input("Вашият отговор:",key ="a")
 if st.button("Провери отговор 2"):
     if user_answer2.strip() == "681":
         st.success("Верен отговор!")
@@ -22,7 +22,7 @@ if st.button("Провери отговор 2"):
 
 # Въпрос 3
 st.write("Къде се намира Айфеловата кула?")
-user_answer3 = st.text_input("Вашият отговор:")
+user_answer3 = st.text_input("Вашият отговор:",key = "s")
 if st.button("Провери отговор 3"):
     if user_answer3.strip().lower() == "париж":
         st.success("Верен отговор!")
@@ -31,7 +31,7 @@ if st.button("Провери отговор 3"):
 
 # Въпрос 4
 st.write("Коя година е започнала Първата световна война?")
-user_answer4 = st.text_input("Вашият отговор:")
+user_answer4 = st.text_input("Вашият отговор:",key = "d")
 if st.button("Провери отговор 4"):
     if user_answer4.strip() == "1914":
         st.success("Верен отговор!")
